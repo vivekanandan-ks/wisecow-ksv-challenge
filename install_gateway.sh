5eafe508fa1cf8dae621d89e11e67ea390a3c877
vivekanandan-ks <ksvdevksv@gmail.com>
2026-02-04 04:29:53 +0530

    k0s implementation done, gh actions for image tag WIP

--- install_gateway.sh
+++ install_gateway.sh
@@ -7,3 +7,6 @@ kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/downloa
 echo "Installing Caddy Gateway Controller..."
 # Using the default Kustomize configuration from the official repo
 kubectl apply -k github.com/caddyserver/gateway/config/default
+
+echo "Installing cert-manager..."
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
